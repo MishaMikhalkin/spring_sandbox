d8b2ad106a22c723d5a42c41b25d17cf21f83a46
Mikhail MIkhalkin <michail.michalkin@gmail.com>
2019-03-15 00:18:43 +0300

    lab2 refactor test

--- src/test/java/lab1/dao/StudentConsoleDaoTest.java
+++ src/test/java/lab1/dao/StudentConsoleDaoTest.java
@@ -3,34 +3,25 @@ package lab1.dao;
 import lab1.domain.Student;
 import lab1.util.AppLanguage;
 import lab1.util.ConsoleUtil;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.context.annotation.Primary;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@RunWith(SpringRunner.class)
-class StudentConsoleDaoTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+public class StudentConsoleDaoTest {
 
     @Configuration
-    static class ConsoleUtilConfuguration {
-
-        @Bean MessageSource messageSource () {
-            return Mockito.mock(MessageSource.class);
-        }
-
-        @Bean AppLanguage language() {
-            return new AppLanguage("ru");
-        }
-
+    static class ConsoleUtilConfiguration {
         @Bean
         public ConsoleUtil consoleUtil() {
-            return Mockito.spy(new ConsoleUtil(language(), messageSource()));
+            return Mockito.mock(ConsoleUtil.class);
         }
 
     }
@@ -44,7 +35,7 @@ class StudentConsoleDaoTest {
         StudentDao studentDao = new StudentConsoleDao(consoleUtil);
 
         Student student = studentDao.load();
-        assertTrue(student.getFirstName().equals("name"));
-        assertTrue(student.getLastName().equals("lastname"));
+        assert(student.getFirstName().equals("name"));
+        assert(student.getLastName().equals("lastname"));
     }
 }
--- src/test/java/lab1/service/StudentServiceImplTest.java
+++ src/test/java/lab1/service/StudentServiceImplTest.java
@@ -3,20 +3,19 @@ package lab1.service;
 import lab1.dao.StudentConsoleDao;
 import lab1.dao.StudentDao;
 import lab1.domain.Student;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-class StudentServiceImplTest {
+public class StudentServiceImplTest {
 
     @Test
-    void register() {
+    public void register() {
         Student compare = new Student("firstname", "lastname");
         StudentDao studentMockDao = Mockito.mock(StudentConsoleDao.class);
         Mockito.when(studentMockDao.load()).thenReturn(compare);
         StudentService studentService = new StudentServiceImpl(studentMockDao);
         Student student = studentService.register();
-        assertTrue(student.equals(compare));
+        assert(student.equals(compare));
     }
 }
